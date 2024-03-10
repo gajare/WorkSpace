03be44233f9dc2ca7074583e1c2845b663b1db57
gajare <gajare147@gmail.com>
2024-03-10 17:43:35 +0530

    linear search program

---
+++ LinearSearch/go.mod
@@ -0,0 +1,3 @@
+module LinearSearch
+
+go 1.22.0
--- LinearSearch/main.go
+++ LinearSearch/main.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello")
+	arr:=[]int{1,6,2,8,3,6,4,5,9,10}
+	target:=7
+	res:=LineraSearch(arr,target)
+	if res!=0{
+		fmt.Println("target Found in the array")
+	}else{
+		fmt.Println("target NOT Found in the array")
+	}
+}
+func LineraSearch(arr[]int,target  int)int{
+	for v:=range arr{
+		if v==target{
+			return v
+		}
+	}
+	return 0
 }
