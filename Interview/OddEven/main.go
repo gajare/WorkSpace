07a5ec1fe6642bcaf0721b09b3057cd6b88e0f5c
gajare <gajare147@gmail.com>
2024-03-11 01:27:30 +0530

    without waitgroup

--- Interview/OddEven/main.go
+++ Interview/OddEven/main.go
@@ -7,29 +7,30 @@ import (
 
 var wg sync.WaitGroup
 
+func main() {
+
+	wg.Add(2)
+	ch := make(chan bool, 5)
+	go Odd(ch)
+	go Even(ch)
+	wg.Wait()
+
+}
+
 func Odd(ch chan bool) {
+	defer wg.Done()
+	for i := 0; i <= 10; i += 2 {
+		
+		ch <- true
+		<-ch
+		fmt.Printf("%d", i)
+	}
+}
+func Even(ch chan bool) {
 	defer wg.Done()
 	for i := 1; i <= 10; i += 2 {
 		ch <- true
-		fmt.Println(i)
-		<-ch // Wait for the signal from the other goroutine before proceeding
-
-	}
-}
-
-func Even(ch chan bool) {
-	defer wg.Done()
-	for i := 2; i <= 10; i += 2 {
-		ch <- true // Send signal (true) to allow the other goroutine to proceed
-		fmt.Println(i)
 		<-ch
-
+		fmt.Printf("%d", i)
 	}
-}
-func main() {
-	wg.Add(2)
-	ch := make(chan bool, 5)
-	go Even(ch)
-	go Odd(ch)
-	wg.Wait()
 }
