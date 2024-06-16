5ef2e35ed9b35c31b1d752a1ba848bcca74f5b5e
amol <gajare147@gmail.com>
2024-06-16 20:51:05 +0530

    all interview has this question

--- Interview/OddEven/main.go
+++ Interview/OddEven/main.go
@@ -2,29 +2,34 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
-func main() {
+var wg sync.WaitGroup
 
-	ch := make(chan bool, 5)
-	go Odd(ch)
-	go Even(ch)
-
-}
-
-func Odd(ch chan bool) {
-
-	for i := 0; i <= 10; i += 2 {
-
-		ch <- true
-		<-ch
-		fmt.Printf("%d", i)
-	}
-}
-func Even(ch chan bool) {
+func PrintNumbers(ch chan bool) {
+	defer wg.Done()
 	for i := 1; i <= 10; i += 2 {
 		ch <- true
-		<-ch
-		fmt.Printf("%d", i)
+		fmt.Println(i)
+		<-ch // Wait for the signal from the other goroutine before proceeding
+
 	}
+}
+
+func PrintAlphabets(ch chan bool) {
+	defer wg.Done()
+	for i := 2; i <= 10; i += 2 {
+		ch <- true // Send signal (true) to allow the other goroutine to proceed
+		fmt.Println(i)
+		<-ch
+
+	}
+}
+func main() {
+	wg.Add(2)
+	ch := make(chan bool, 5)
+	go PrintAlphabets(ch)
+	go PrintNumbers(ch)
+	wg.Wait()
 }
