f668a1f0e7a6368fc17209d6d318514e08aaf30e
amol <gajare147@gmail.com>
2024-06-17 21:22:02 +0530

    queue

---
+++ queue/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+type Queue struct {
+	data []int
+}
+
+func (q *Queue) Add(item int) {
+	q.data = append(q.data, item)
+}
+func (q *Queue) Delete() int {
+	item := q.data[0]
+	q.data = q.data[1:]
+	return item
+}
+
+func (q *Queue) Dispaly() {
+	for _, v := range q.data {
+		fmt.Print(v)
+	}
+}
+func main() {
+	queue := Queue{}
+	queue.Add(11)
+	queue.Add(22)
+	queue.Add(33)
+	queue.Dispaly()
+	fmt.Println("\n", queue.Delete())
+	queue.Dispaly()
+	fmt.Println("\n", queue.Delete())
+	queue.Dispaly()
+
+}
