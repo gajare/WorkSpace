07d9bded53d16ddf3a1cc97f00b693f683daefa7
Amol Pratap Gajare <31128887+gajare@users.noreply.github.com>
2024-06-18 06:24:03 -0800

    Update main.go

--- oddEven/main.go
+++ oddEven/main.go
@@ -7,29 +7,28 @@ import (
 
 var wg sync.WaitGroup
 
-func PrintNumbers(ch chan bool) {
+func Odd(ch chan bool) {
 	defer wg.Done()
-	for i := 1; i <= 10; i += 2 {
-		ch <- true
-		fmt.Println(i)
-		<-ch // Wait for the signal from the other goroutine before proceeding
-
+	for i := 1; i < 10; i += 2 {
+		ch <- true // Send signal (true) to allow the other goroutine to proceed
+		fmt.Println(" odd :", i)
+		<-ch
 	}
 }
 
-func PrintAlphabets(ch chan bool) {
+func Even(ch chan bool) {
 	defer wg.Done()
 	for i := 2; i <= 10; i += 2 {
 		ch <- true // Send signal (true) to allow the other goroutine to proceed
-		fmt.Println(i)
+		fmt.Println("even :", i)
 		<-ch
-
 	}
 }
 func main() {
 	wg.Add(2)
 	ch := make(chan bool, 5)
-	go PrintAlphabets(ch)
-	go PrintNumbers(ch)
+
+	go Even(ch)
+	go Odd(ch)
 	wg.Wait()
 }
