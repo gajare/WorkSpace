a0695bac2cc657e23d8f846eb359b2d3d3a23621
amol <gajare147@gmail.com>
2024-06-27 00:54:36 +0530

    serlize12345678910

--- oddEven/serial/main.go
+++ oddEven/serial/main.go
@@ -2,38 +2,33 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
-func main() {
-	oddCh := make(chan bool)
-	evenCh := make(chan bool)
-	done := make(chan bool)
+var wg sync.WaitGroup
 
-	go printOdd(oddCh, evenCh, done)
-	go printEven(oddCh, evenCh, done)
-
-	oddCh <- true // Start the sequence with the odd number
-
-	<-done // Wait for the completion signal
-}
-
-func printOdd(oddCh, evenCh, done chan bool) {
-	for i := 1; i <= 10; i += 2 {
-		<-oddCh
-		fmt.Println(i)
-		evenCh <- true
+func Odd(ch chan int) {
+	defer wg.Done()
+	for i := 1; i < 10; i += 2 {
+		ch <- i
+		fmt.Println(" odd :", i)
+		<-ch
 	}
-	done <- true
 }
 
-func printEven(oddCh, evenCh, done chan bool) {
+func Even(ch chan int) {
+	defer wg.Done()
 	for i := 2; i <= 10; i += 2 {
-		<-evenCh
-		fmt.Println(i)
-		if i >= 10 {
-			done <- true
-			return
-		}
-		oddCh <- true
+		<-ch
+		fmt.Println("even :", i)
+		ch <- i
 	}
+}
+func main() {
+	wg.Add(2)
+	ch := make(chan int, 5)
+
+	go Even(ch)
+	go Odd(ch)
+	wg.Wait()
 }
